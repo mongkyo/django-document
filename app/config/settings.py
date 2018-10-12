359831d31cd43e788ef810d243899a9ae25f9c05
JungMongKyo <dreamong91@gmail.com>
2018-10-12 15:00:04 +0900

    proxy모델 코드 추가

--- app/config/settings.py
+++ app/config/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'inheritance.abstract_base_classes',
     'inheritance.abc_other',
     'inheritance.multi_table',
+    'inheritance.proxy',
 
     'relation.foreignkey',
     'relation.many_to_many',
